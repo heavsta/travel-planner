ca00d705a49a85b1c20bda67c8a3ac809798f355
heavsta <francoisleduc@ymail.com>
2020-07-17 21:50:10 +0200

    added global variables & the main function, all skeleton already built

---
+++ src/client/js/app.js
@@ -0,0 +1,49 @@
+/**
+ * Global Variables
+ */
+
+const geonamesUrl = 'http://api.geonames.org/searchJSON?q=';
+const geonamesApiKey = process.env.GEONAMES_API_ID;
+
+const weatherbitUrl = 'https://cors-anywhere.herokuapp.com/http://api.weatherbit.io/v2.0/forecast/daily';
+const weatherbitApiKey = process.env.WEAHTERBIT_API_ID;
+
+const pixabayURl = 'https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?key=';
+const pixabayApikey = process.env.PIXABAY_API_ID;
+
+const submit = document.getElementById('generate');
+
+
+/**
+ * Main function
+ */
+
+// Event listener
+submit.addEventListener('click', saveTrip);
+
+function saveTrip(event) {
+    //preventing default action
+    event.preventDefault();
+
+    //getting user input into variables
+    const cityInput = document.getElementById('city').value;
+    const dateInput = document.getElementById('date').value;
+
+    getCoordinates(geonamesUrl, cityInput, geonamesApiKey)
+        //using coordinates to create weather variable
+        .then((coordinates) => {
+            const latitude = coordinates.geonames[0].lat;
+            const longitude = coordinates.geonames[0].lng;
+            const weatherData = getWeather(latitude, longitude, dateInput);
+            return weatherData;
+        })
+        //add all the data to the POST request
+        .then((weather) => {
+            const allData = postData('http://localhost:8080/add', {cityInput, dateInput, weather: weatherData.data[0].temp});
+            return allData;
+        })
+        //update UI
+        .then(function(allData) {
+            updateUI(allData);
+        });
+}
--- src/server/server.js
+++ src/server/server.js
@@ -32,3 +32,21 @@ app.get('/', function(req, res) {
 });
 
 const server = app.listen(port, () => {console.log(`running succesfully on local host: ${port}`)});
+
+
+//GET route
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+  });
+
+
+//POST route adds incoming projectData
+app.post('/add', function (req, res) {
+    projectData.locationAnswer = req.body.location;
+    projectData.dateAnswer = req.body.dateAnswer;
+    projectData.weather = req.body.weather;
+    projectData.daysUntil = req.body.daysUntil;
+    res.send(projectData);
+});
+    
+module.exports = app;
