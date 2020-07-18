29cfd57184c456967440e2b31eee6d033ddfb42b
heavsta <francoisleduc@ymail.com>
2020-07-18 11:45:04 +0200

    changed app.spec.js

--- __test__/app.spec.js
+++ __test__/app.spec.js
@@ -1,9 +1,10 @@
 import { saveTrip } from "../src/client/js/app";
 
-
-//Main Function
-describe('Test, the function saveTrip should be a function', () => {
-    test('It should return true', async () => {
-        expect(typeof saveTrip).toBe("function");
+describe('Tests saveTrip', () => {
+    test('Checks if saveTrip is a function', async () => {
+      expect(typeof saveTrip).toBe("function");
+    });
+    test('Checks if saveTrip function is defined', () => {
+        expect(saveTrip).toBeDefined();
     });
 });
